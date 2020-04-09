30bf83307f25ce0297d6d70708d72f189e48b80f
nemo158 <chunshuyulesa@126.com>
2020-04-09 17:22:53 +0800

    test

--- mulTest.py
+++ mulTest.py
@@ -1,8 +1,8 @@
 import numpy as np
 import multiple as mul
 
-first=np.random.randint(1,100,size=(3,3))
-second=np.random.randint(1,100,size=(3,3))
+first=np.random.randint(1,100,size=(4,4))
+second=np.random.randint(1,100,size=(4,4))
 back=mul.multiply_print(first,second)
 if (back==np.multiply(first,second)).all():
     print("这俩一样的，验证OK")
